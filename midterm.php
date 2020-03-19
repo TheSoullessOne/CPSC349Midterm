2c14e0c70acd4b604a6568bbd8e2293a23454164
Eric <eric.edelman24@gmail.com>
2020-03-18 20:15:14 -0700

    last fix

--- midterm.php
+++ midterm.php
@@ -2,10 +2,10 @@
 // <!--                                                        #2                                                                               -->
 // <!-- (10 pts) Write the php code to connect to a remote database, and display an error message if the connection fails.                      -->
 $connection = mysqli_connect("localhost", "root", "", "classregistration");
-  if (!$connection) {
-    echo "Cannot connect to MySQL.". mysqli_connect_error();
-    exit();
-  }
+if (!$connection) {
+  echo "Cannot connect to MySQL.". mysqli_connect_error();
+  exit();
+}
 
 
 
@@ -21,7 +21,7 @@ $encryptpasswd = sha1($userPasswd); // Note: sha1 encryption is obsolete
 $db_name = $row->admin_name;
 
 if ($db_userid != $userid || $db_password != $encryptpasswd) {
-$query = "INSERT INTO administrator(admin_id, admin_password, admin_name) 
+    $query = "INSERT INTO administrator(admin_id, admin_password, admin_name) 
               VALUES('$userid', '$encryptpasswd', '$name')";
     mysqli_query($connection, $query);    if (!$result) {
         echo ("Insert to administrator failed. ". mysqli_error($connection));
